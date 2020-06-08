fea676618f0f6afdc76a0dfb639befe7c3a7d35c
Gabriel Onike <onikegabriel@gmail.com>
2020-06-08 11:03:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,18 +4,18 @@ class TicTacToe
  end
 
 def display_board
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move( index, token)
+def move(board, index, token)
   #current_player = "X"
   board[index] = token
 end
